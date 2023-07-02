27dfc456304e4b9aad3ea6ff1c5d34db40c26be3
Abe-alt <yousfi.abdou@gmail.com>
2023-07-02 09:27:56 +0100

    serving index.html as fileserver

--- main.go
+++ main.go
@@ -19,20 +19,23 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func main() {
+	const port = "8080"
+	const filepathRoot = "."
 	// Create a new http.ServeMux
 	mux := http.NewServeMux()
-
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	//Wrap that mux in a custom middleware function that adds CORS headers to the response (see the tip below on how to do that).
 	corsMux := middlewareCors(mux)
 
 	//Create a new http.Server and use the corsMux as the handler
-	srv := http.Server{Addr: ":8080", Handler: corsMux}
+	srv := &http.Server{Addr: ":" + port, Handler: corsMux}
+	//&http.Server{} creates a new http.Server struct and returns a pointer to that struct.
 
 	//Use the server's ListenAndServe method to start the server
-	log.Printf("serving on port 8080:")
+	log.Printf("serving files from %s on port %s:", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 	//if err := http.ListenAndServe(":8080", corsMux); err != nil {
 	//	log.Fatal(err)
-	//
 	//}
+
 }
