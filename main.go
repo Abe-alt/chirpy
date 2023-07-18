e35fd74ae7e113063547fcb61e6e49bff0d18015
Abe-alt <yousfi.abdou@gmail.com>
2023-07-18 03:52:55 +0100

    cleaned

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte("ok"))
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)                    // 200
+	w.Write([]byte(http.StatusText(http.StatusOK))) // ("ok")
 }
