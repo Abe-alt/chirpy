04eaa623d2eac13f5380f1c80939888a4f3e36fb
Abe-alt <yousfi.abdou@gmail.com>
2023-07-19 02:56:11 +0100

    logs handler

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ type apiconfig struct {
 	fileserverHits int
 }
 
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (cfg *apiconfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		cfg.fileserverHits++
@@ -26,11 +33,18 @@ func main() {
 
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir(filepathRoot))
-	cfg := cfgApi.middlewareMetricsInc(files)
+
+	filesWithLog := middlewareLog(files)
+
+	cfg := cfgApi.middlewareMetricsInc(filesWithLog)
+
 	//mux.Handle("/app/", http.StripPrefix("/app/", files))
 	mux.Handle("/app/", http.StripPrefix("/app/", cfg))
 	mux.HandleFunc("/healthz", healthz)
 	mux.HandleFunc("/metrics", cfgApi.metrics)
+
+	// Register the logsHandler function for the "/logs" path
+	//mux.HandleFunc("/logs", logsHandler)
 
 	corsMux := middlewareCors(mux)
 
@@ -53,4 +67,12 @@ func (cfg *apiconfig) metrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf("Hits:%v", cfg.fileserverHits)))
+}
+func logsHandler(w http.ResponseWriter, r *http.Request) {
+	// Here, you can write the log content or any other response you want.
+	// For example, you can read a log file and write its content to the response.
+	logContent := "This is a log entry.\nMore log lines..."
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(logContent))
 }
